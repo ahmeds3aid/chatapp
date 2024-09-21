11e912863953b743280254a0f39ea3822a1049e9
ahmedsaid <as1241846@gmail,com>
2024-09-21 22:54:55 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:chatapp/features/auth/presentation/views/login_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Chatapp());
+}
+
+class Chatapp extends StatelessWidget {
+  const Chatapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: LoginView(),
+    );
+  }
+}
